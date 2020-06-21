c4c5b372b34ddf98b51314b3924b66ef4fbc316d
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-21 11:01:35 -0300

    :zap: create serverName

---
+++ src/components/ServerName/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import * as S from './styled'
+
+const ServerName: React.FC = () => (
+  <S.ServerNameWrapper>
+    <S.Title>FelipeSuri Server</S.Title>
+
+    <S.ExpandIcon />
+  </S.ServerNameWrapper>
+)
+
+export default ServerName
