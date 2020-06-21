68f512712100d1236d641dccb18fd85a4c0f4494
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-21 11:01:52 -0300

    :sparkles: implemented serverName

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 import * as S from './styled'
 
 import ServerList from '../ServerList'
+import ServerName from '../ServerName'
 
 const Layout: React.FC = () => (
   <S.Grid>
     <ServerList />
+    <ServerName />
   </S.Grid>
 )
 
