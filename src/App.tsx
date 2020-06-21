7e28540e646730f4e09a04610aea2c3f599e8009
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-21 09:23:21 -0300

    :zap: implemented layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
+import Layout from './components/Layout'
 import GlobalStyles from './styles/globalStyles'
 
 const App = () => (
-  <>
+  <Layout>
     <h1>FelipeSuri</h1>
 
     <GlobalStyles />
-  </>
+  </Layout>
 )
 
 export default App
