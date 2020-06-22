c8ec202c5a38d574c7c9ac25eac720a6fdbd5c48
Felipe Suri <felipeheredia1234@gmail.com>
2020-06-22 08:42:46 -0300

    :sparkles: implemented ChannelList

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,12 +5,14 @@ import * as S from './styled'
 import ServerList from '../ServerList'
 import ServerName from '../ServerName'
 import ChannelInfo from '../ChannelInfo'
+import ChannelList from '../ChannelList'
 
 const Layout: React.FC = () => (
   <S.Grid>
     <ServerList />
     <ServerName />
     <ChannelInfo />
+    <ChannelList />
   </S.Grid>
 )
 
